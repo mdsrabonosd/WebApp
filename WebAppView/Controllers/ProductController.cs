8974176bc01530c60237ba9956ea8748911b29ef
MdSrabon <mdsrabon.cse@gmail.com>
2026-01-31 01:21:21 +0600

    ViewBag,ViewData,ModelObj

--- WebAppView/Controllers/ProductController.cs
+++ WebAppView/Controllers/ProductController.cs
@@ -8,5 +8,47 @@ namespace WebAppView.Controllers
         {
             return View();
         }
+
+        //Pass data using ViewBag
+        public IActionResult Book()
+        {
+            string[] Booklist = {
+                "Bangla",
+                "English",
+                "Physics"
+           } ;
+            ViewBag.message = "This is booklist";
+            ViewBag.Mybooks = Booklist;
+            return View();
+        }
+
+        //Pass data using ViewData
+        public IActionResult Mobile()
+        {
+            string[] Mobleset =
+            {
+              "samsung",
+              "Huaway",
+              "Vivo",
+              "itel",
+              "Mi"
+            };
+            ViewData["srabon"] = Mobleset;
+            return View();
+        }
+
+        //Pass data using Model Object
+        public IActionResult Friute()
+        {
+            string[] Fruits =
+            {
+               "Banana",
+               "Mango",
+               "Apple",
+               "Orenge",
+               "Cool"
+            };
+            return View(Fruits);
+        }
     }
 }
