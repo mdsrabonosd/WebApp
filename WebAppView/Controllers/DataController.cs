26f43b89d82774e8f605d93c5ef487f9a566ac35
MdSrabon <mdsrabon.cse@gmail.com>
2026-02-01 12:52:24 +0600

    File information show

---
+++ WebAppView/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppView.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FileInfoA()
+        {
+            return View();
+        }
+        public IActionResult FileInfoB()
+        {
+            return View();
+        }
+    }
+}
