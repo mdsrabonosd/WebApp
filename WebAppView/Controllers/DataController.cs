cbce7b342ab10f20c479b4ec518547ef46f6a8d8
MdSrabon <mdsrabon.cse@gmail.com>
2026-02-02 23:52:37 +0600

    File Info

--- WebAppView/Controllers/DataController.cs
+++ WebAppView/Controllers/DataController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppView.ViewModel;
 
 namespace WebAppView.Controllers
 {
@@ -14,6 +15,23 @@ namespace WebAppView.Controllers
         }
         public IActionResult FileInfoB()
         {
+            return View();
+        }
+
+        //show the File information  using Model
+        public IActionResult FileInfoInfo()
+        {
+            int i = 1;
+            var folder = @"C:\Users\user\Documents\Materials";
+            string[] files = Directory.GetFiles(folder);
+            List<FileInformation> list = new List<FileInformation>();
+            foreach (string item in files)
+            {
+                FileInformation Finfo = new FileInformation();
+                Finfo.Id = i;
+                Finfo.FileName=Path.GetFileNameWithoutExtension(item);
+                Finfo.FileExtention=Path.GetExtension(item);
+            }
             return View();
         }
     }
---
+++ WebAppView/ViewModel/FileInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAppView.ViewModel
+{
+    public class FileInformation
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExtention { get; set; }
+        public long Filesize { get; set; }
+        public string FilePath { get; set; }
+       
+    }
+}
