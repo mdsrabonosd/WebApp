81b78b8db419825d99d9f63ca6d7398c2139e196
MdSrabon <mdsrabon.cse@gmail.com>
2026-02-06 18:38:59 +0600

    one method use from another method and change color by viewbag

--- WebAppView/Controllers/DataController2.cs
+++ WebAppView/Controllers/DataController2.cs
@@ -6,7 +6,14 @@ namespace WebAppView.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            string folder = @"C:\temp";
+            string filename = "syslog.txt";
+            string fpath = Path.Join(folder, filename);
+            string[] lines = System.IO.File.ReadAllLines(fpath);
+            ViewBag.color = "bg-primary";
+            ViewBag.msg = "show log file from Index";
+            return View("Showlog",lines);
+            
         }
         public IActionResult Showlog()
         {
@@ -14,6 +21,10 @@ namespace WebAppView.Controllers
             string filename = "syslog.txt";
             string fpath = Path.Join(folder, filename);
             string[] lines=System.IO.File.ReadAllLines(fpath);
+            ViewBag.color = "bg-danger";
+
+            ViewBag.msg = "show log file from Showlog";
+
             return View(lines);
         }
     }
