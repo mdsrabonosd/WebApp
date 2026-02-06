1f470cf7777945b155d6abf15bfa21a346593db0
MdSrabon <mdsrabon.cse@gmail.com>
2026-02-06 18:10:34 +0600

    Show LogFile

---
+++ WebAppView/Controllers/DataController2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppView.Controllers
+{
+    public class DataController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Showlog()
+        {
+            string folder = @"C:\temp";
+            string filename = "syslog.txt";
+            string fpath = Path.Join(folder, filename);
+            string[] lines=System.IO.File.ReadAllLines(fpath);
+            return View(lines);
+        }
+    }
+}
